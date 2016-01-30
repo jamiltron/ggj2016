bb7e4cc8e2463aa5447a248a73849342e2b0e069
Justin Hamilton <justinanthonyhamilton@gmail.com>
2016-01-30 14:44:43 -0800

    Adding continue button

---
+++ Assets/Scripts/CustomerPitchUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CustomerPitchUI : MonoBehaviour {
+
+  public GameObject helpButton;
+  public GameObject dontHelpButton;
+  public GameObject continueButton;
+
+  public void EnableHelpButtons() {
+    helpButton.SetActive(true);
+    dontHelpButton.SetActive(true);
+  }
+
+  public void DisableHelpButtons() {
+    helpButton.SetActive(false);
+    dontHelpButton.SetActive(false);
+  }
+
+  public void EnableContinueButton() {
+    continueButton.SetActive(true);
+  }
+
+  public void DisableContinueButton() {
+    continueButton.SetActive(false);
+  }
+
+
+}
