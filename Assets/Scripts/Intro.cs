0c50e26485b356ef08422bfb621704a02afbea4e
dilarasemerci <dilarasem@gmail.com>
2016-01-29 23:21:42 -0800

    Added intro and game scenes

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnStartGameClicked()
+	{
+		Application.LoadLevel ("GameScene");
+	}
+}
