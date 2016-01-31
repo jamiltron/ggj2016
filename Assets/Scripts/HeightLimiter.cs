797d00ee5ff34be968fa0e38d7332b6c27bdb327
Justin Hamilton <justinanthonyhamilton@gmail.com>
2016-01-31 11:16:08 -0800

    Adding HeightLimiter

---
+++ Assets/Scripts/HeightLimiter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeightLimiter : MonoBehaviour {
+
+  public float minHeight = -0.5f;
+
+  private Collider2D col;
+  private Transform trans;
+
+  void Start() {
+    col = GetComponent<Collider2D>();
+    trans = GetComponent<Transform>();
+  }
+
+  void Update() {
+
+  }
+
+  void LateUpdate() {
+    float halfY = col.bounds.size.y / 2;
+    Debug.Log(trans.position.y - halfY);
+    if (trans.position.y - halfY < minHeight) {
+      Debug.Log("LIMITING!");
+      Vector3 vec = trans.position;
+      vec.y = minHeight + halfY;
+      trans.position = vec;
+    }
+  }
+}
