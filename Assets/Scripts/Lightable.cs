ea46bca8cab6d7306510ad965314a40ff5fc5174
Justin Hamilton <justinanthonyhamilton@gmail.com>
2016-01-31 11:48:34 -0800

    Adding lightable candles

---
+++ Assets/Scripts/Lightable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightable : MonoBehaviour {
+
+  public GameObject flame;
+  public LayerMask grabMask;
+
+  void Update() {
+    if (Input.GetMouseButtonDown(0)) {
+      Debug.Log("mouse down!");
+      Vector3 objScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
+      Vector3 currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, objScreenPosition.z);
+      Vector3 vec = Camera.main.ScreenToWorldPoint(currentScreenSpace);
+
+      RaycastHit2D hit = Physics2D.Raycast(vec, Vector2.right, 0.1f, grabMask);
+      if (hit.transform != null) {
+        if (hit.transform.gameObject == this.gameObject) {
+          ToggleLight();
+        }
+      }
+    }
+  }
+
+  public void ToggleLight() {
+    if (flame.activeSelf) {
+      flame.SetActive(false);
+    } else {
+      flame.SetActive(true);
+    }
+  }
+}
